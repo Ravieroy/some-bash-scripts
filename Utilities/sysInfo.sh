91cdb333367f53c4eb0704fcedcc868829c4e983
Ravi Roy <81288438+Ravieroy@users.noreply.github.com>
2021-04-12 13:57:49 +0530

    Update sysInfo.sh

--- Utilities/sysInfo.sh
+++ Utilities/sysInfo.sh
@@ -4,22 +4,43 @@
 # You require acpi installed to display battery info. It would work either way
 # Made by Ravi roy (https://github.com/ravieroy)
 
+
+#----For color-----
+
+txtred=$(tput setaf 1)
+txtgrn=$(tput setaf 2)
+txtylw=$(tput setaf 3)
+txtblu=$(tput setaf 4)
+txtpur=$(tput setaf 5)
+txtcyn=$(tput setaf 6)
+txtrst=$(tput sgr0)
+
+
+message(){
+    echo "${txtred}.........ATTENTION.......................${txtrst}"
+    echo "${txtylw}If your distribution is anything other than Debian/Ubuntu, Please make sure you have acpi and dmidecode installed.${txtrst}"
+    echo "${txtylw}For Debian/Ubuntu based distros you will be prompted to install directly.${txtrst}"
+    sleep 7
+}
+
+#--------------------
+
 #This function checks for root access 
 
 chk_root() {
 	local meid=$(id -u)
 	if [ $meid -ne 0 ]; then
-		echo "You are not root user. This will work best if you are root."
+		echo "${txtred}ATTENTION: You are not root user. This will work best if you are root.${txtrst}"
 		exit 999
     fi
 }
 
+#This is required in system which doesn't have dmidecode by default
 
-#This installs acpi for battery info
-install_acpi(){
+install_dmidecode(){
 
-if ! acpi; then
-    echo -e " acpi needs to be installed to display battery information. Do you want to install (Y/n)? \c"
+if ! dmidecode --version; then
+    echo -e "${txtred}ATTENTION: ${txtrst}dmidecode needs to be installed for complete execution. Do you want to install(Debian/Ubuntu) (Y/n)? \c"
     read input
     if [ -z "$input" ]
     then
@@ -27,42 +48,103 @@ if ! acpi; then
     fi
     case $input in
         "Y" | "y" )
-        sudo apt -y install acpi
-        echo "Install complete" ;;
+        if [ -z $(hostname -I | awk '{print $1}') ]
+        then
+            echo "${txtred}Cannot Install dmidecode. Your system has no internet connection${txtrst}" 
+            echo "${txtylw}Continuing without dmidecode${txtrst}"
+            sleep 3
+        else
+            sudo apt -y install dmidecode
+            echo "${txtylw}Installation complete: Displaying sytem information now${txtrst}"
+            sleep 3 
+        fi ;;
         "N" | "n" )
-         echo "Can't show battery info"  ;;
-    esac
-fi
+         echo "${txtred}Will display some errors!!${txtrst}"
+         sleep 2 ;;
+
+         * )
+
+         echo "${txtred}Please make a correct choice.Thank You.${txtrst}"
+         exit 999
+
+    esac 
+fi 
+}
+
+#------------------------
+
+#This installs acpi for battery info
+install_acpi(){
+
+if ! acpi; then
+    echo -e "${txtred}ATTENTION: ${txtrst}acpi needs to be installed to display battery information. Do you want to install(Debian/Ubuntu) (Y/n)? \c"
+    read input
+    if [ -z "$input" ]
+    then
+            input=Y
+    fi
+    case $input in
+        "Y" | "y" )
+        if [ -z $(hostname -I | awk '{print $1}') ]
+        then
+            echo "${txtred}Cannot Install acpi. Your system has no internet connection${txtrst}" 
+            echo "${txtylw}Continuing without acpi${txtrst}" 
+            sleep 3
+        else
+            sudo apt install acpi -y
+            echo "${txtylw}Installation complete: Displaying sytem information now${txtrst}"
+            sleep 1
+            
+        fi ;;
+        "N" | "n" )
+         echo "${txtred}Can't show battery info${txtrst}" 
+         sleep 2 ;;
+
+         * )
+
+         echo "${txtred}Please make a correct choice.Thank You.${txtrst}"
+         exit 999
+    esac 
+fi 
 }
 
 # To check for empty slot in memory
 mem_slot(){
 lshw -short -C memory | grep -i empty
 if [ $? -ne 0 ]; then
-    echo "You do not have any empty memory slots"
+    echo "${txtgrn}You do not have any empty memory slots${txtrst}"
 else
 	
-    echo "You have empty memory slot"
+    echo "${txtgrn}You have empty memory slot${txtrst}"
 fi
 }
+
+
+# Define variables
+LSB=/usr/bin/lsb_release
+
+
+
+
+
 
 # This function contains all the process 
 
 process(){
 
-echo "---------------------------------SYSTEM------------------------------------------------"
+echo "${txtylw}--------------------------------SYSTEM-------------------------------------------------${txtrst}"
 echo " "
 
 dmidecode | grep -A4 '^System Information'  #provides system information
 
 echo " "
-echo "----------------------------------32/64------------------------------------------------" 
+echo "${txtylw}--------------------------------ARCHITECTURE-------------------------------------------${txtrst}"
 
 
 echo "System $(lscpu | grep -i architecture)"  # architecture 64 or 32 bits
 
 echo " "
-echo "--------------------------------BATTERY------------------------------------------------" 
+echo "${txtylw}----------------------------------BATTERY----------------------------------------------${txtrst}" 
 
 
 echo "Battery : $(dmidecode | grep -i design\ capacity)" # battery capacity
@@ -70,11 +152,13 @@ echo "Battery : $(dmidecode | grep -i design\ capacity)" # battery capacity
 if acpi; then
     echo " " 
 else
-    echo "acpi not found!! Can't display battery capacity. "
+    echo "${txtred}acpi not found!! Can't display battery capacity.${txtrst}"
+    echo ""
 fi
 
 echo " "
-echo "----------------------------------SCREEN-----------------------------------------------"
+
+echo "${txtylw}----------------------------------SCREEN-----------------------------------------------${txtrst}"
 
 
 #echo "Screen Dimension: $(xdpyinfo | awk '/dimensions/{print $2}')" #Screen dimension
@@ -84,103 +168,107 @@ echo "Screen Resolution and Refresh rate: $(xrandr | grep -i "\*") "
 
 
 echo " "
-echo "----------------------------------LANGUAGE---------------------------------------------"
+echo "${txtylw}----------------------------------LANGUAGE---------------------------------------------${txtrst}"
 
 
 echo $(dmidecode -t bios | grep -i installed\ language) #installed language
 
 echo " "
-echo "------------------------------------OS-------------------------------------------------"
+echo "${txtylw}------------------------------------OS-------------------------------------------------${txtrst}"
 
 
-echo "Installed OS $(lsb_release -a | grep -i description)" # OS version 
+echo "Operating system : $(uname)"
+[ -x $LSB ] && $LSB -a || echo "$LSB command is not insalled (set \$LSB variable)"
 
 echo " "
-echo "---------------------------------------------------------------------------------------"
+echo "${txtylw}---------------------------------------------------------------------------------------${txtrst}"
 
 
 lscpu | grep -i mhz #CPU maximum in mHz
 
 echo " "
-echo "-----------------------MAXIMUM MEMORY FOR HARDWARE-------------------------------------"
+echo "${txtylw}-----------------------MAXIMUM MEMORY FOR HARDWARE-------------------------------------"${txtrst}
 
 
 dmidecode -t memory | grep -i maximum\ capacity #max memory installable
 
 echo " "
-echo "--------------------------EMPTY SLOT---------------------------------------------------"
+echo "${txtylw}--------------------------EMPTY SLOT---------------------------------------------------${txtrst}"
 
 mem_slot # function to check if there is space for memory stick
 
 echo " "
-echo "--------------------------------RAM INFO-----------------------------------------------"
+echo "${txtylw}--------------------------------RAM INFO-----------------------------------------------${txtrst}"
 
 free -h --si  #current memory use
 
 echo " "
-echo "--------------------------------DISK SIZE----------------------------------------------"
+echo "${txtylw}--------------------------------DISK SIZE----------------------------------------------${txtrst}"
 
 echo "Your disk $(lshw -c disk | grep -i size:)" #Disk size
 
 echo " "
-echo "-------------------------------RELEASE DATE--------------------------------------------"
+echo "${txtylw}-------------------------------RELEASE DATE--------------------------------------------${txtrst}"
 
 
 dmidecode -t bios | grep -i release\ date # Release date 
 
 echo " "
-echo "------------------------------------ROM------------------------------------------------"
+echo "${txtylw}------------------------------------ROM------------------------------------------------${txtrst}"
 
 
 dmidecode -t bios | grep -i rom\ size #ROM Size
 
 echo " "
-echo "---------------------------------------------------------------------------------------"
+echo "${txtylw}---------------------------------------------------------------------------------------${txtrst}"
 
 
 dmidecode -t bios | grep -i usb\ legacy # is usb legacy supported
 
 echo " "
-echo "----------------------------------UEFI-------------------------------------------------"
+echo "${txtylw}----------------------------------UEFI-------------------------------------------------${txtrst}"
 
 
 dmidecode -t bios | grep -i uefi # is uefi supported 
 
 echo " "
-echo "---------------------------------CPU---------------------------------------------------"
+echo "${txtylw}---------------------------------CPU---------------------------------------------------${txtrst}"
 
 
 lshw -C cpu | grep -i configuration # cores threads
 
 echo " "
-echo "--------------------------------PROCESSOR----------------------------------------------"
+echo "${txtylw}--------------------------------PROCESSOR----------------------------------------------${txtrst}"
 
 
 lshw -C cpu | grep -i product # Processor 
 
 echo " "
-echo "------------------------------IP Address-----------------------------------------------"
+echo "${txtylw}------------------------------IP Address-----------------------------------------------${txtrst}"
 
 if [ -z $(hostname -I | awk '{print $1}') ]
 then
-    echo "Cannot display IP address. Your system has no internet connection"
+    echo "${txtred}Cannot display IP address. Your system has no internet connection${txtrst}"
 else
-    echo "Your IP address for ssh connection : $(hostname -I | awk '{print $1}')"
+    echo "${txtcyn}Your IP address for ssh connection : $(hostname -I | awk '{print $1}')${txtrst}"
 fi
 
 echo " "
-echo "----------------------------------SSH--------------------------------------------------"
+echo "${txtylw}----------------------------------SSH--------------------------------------------------${txtrst}"
 
 
 echo " ssh service status"
 systemctl status ssh | grep -i active # ssh active/inactive
 
 echo " "
-echo "---------------------------------------------------------------------------------------"
+echo "${txtred}-----------------------------------DONE-------------------------------------------------${txtrst}"
 }
 
 
 chk_root
+clear
+message
 install_acpi
+install_dmidecode
 clear
 process
